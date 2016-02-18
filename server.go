2d9f6e324644cdf79c8079f1d6de77ca92777543
cassiobotaro <cassiobotaro@gmail.com>
2016-02-18 16:24:41 -0200

    add input and template

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"strings"
@@ -15,15 +16,81 @@ func sayHelloName(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(r.Form["url_long"])
 	for k, v := range r.Form {
 		fmt.Println("key: ", k)
-		fmt.Println("val:", strings.Join(v, ""))
+		fmt.Println("val: ", strings.Join(v, ""))
 	}
-	fmt.Fprintf(w, "Hello astaxie!")
+	fmt.Fprintf(w, "Hello, astaxie!")
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method: ", r.Method)
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("login.gtpl")
+		t.Execute(w, nil)
+	} else {
+		r.ParseForm()
+		fmt.Println("username: ", r.Form["username"])
+		fmt.Println("password: ", r.Form["password"])
+	}
 }
 
 func main() {
 	http.HandleFunc("/", sayHelloName)
+	http.HandleFunc("/login", login)
 	err := http.ListenAndServe(":9090", nil)
 	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
+		log.Fatal("LsitenAndServe: ", err)
 	}
 }
+
+// import (
+// 	"fmt"
+// 	"net/http"
+// )
+
+// type MyMux struct{}
+
+// func (p *MyMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+// 	if r.URL.Path == "/" {
+// 		sayHelloName(w, r)
+// 		return
+// 	}
+// 	http.NotFound(w, r)
+// 	return
+// }
+
+// func sayHelloName(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintf(w, "Hello myroute!")
+// }
+
+// func main() {
+// 	mux := &MyMux{}
+// 	http.ListenAndServe(":9090", mux)
+// }
+// --------------------------------------------------------------
+// import (
+// 	"fmt"
+// 	"log"
+// 	"net/http"
+// 	"strings"
+// )
+
+// func sayHelloName(w http.ResponseWriter, r *http.Request) {
+// 	r.ParseForm()
+// 	fmt.Println(r.Form)
+// 	fmt.Println("path", r.URL.Path)
+// 	fmt.Println("scheme", r.URL.Scheme)
+// 	fmt.Println(r.Form["url_long"])
+// 	for k, v := range r.Form {
+// 		fmt.Println("key: ", k)
+// 		fmt.Println("val:", strings.Join(v, ""))
+// 	}
+// 	fmt.Fprintf(w, "Hello astaxie!")
+// }
+
+// func main() {
+// 	http.HandleFunc("/", sayHelloName)
+// 	err := http.ListenAndServe(":9090", nil)
+// 	if err != nil {
+// 		log.Fatal("ListenAndServe: ", err)
+// 	}
+// }
